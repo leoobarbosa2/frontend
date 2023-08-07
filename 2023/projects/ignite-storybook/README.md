1a00ce091994435c846596ecad245a7497f0fc45
leoobarbosa2 <leonardo.pb@icloud.com>
2023-08-07 20:24:46 -0300

    re-uploading more frontend projects

---
+++ 2023/projects/ignite-storybook/README.md
@@ -0,0 +1,16 @@
+# Monorepo
+
+- Prática de manter vários projetos dentro de um mesmo repositório (Mesmo espaço de trabalho)
+- Utilizado com projetos dependentes entre sí.
+
+# Components
+
+- [] Text
+- [] Text
+- [] Text
+- [] Text
+- [] Text
+- [] Text
+- [] Text
+- [] Text
+- [] Text
---
+++ 2023/projects/ignite-storybook/packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,23 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Box, BoxProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+  args: {
+    children: (
+      <>
+        <span>Testando elemento Box</span>
+      </>
+    ),
+  },
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+    },
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
